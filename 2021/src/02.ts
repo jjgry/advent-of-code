6a7692b7ecb6b4e24ac5da386c6acb44793d5f68
JJ Gray <git@jjgry.com>
2021-12-05 23:01:11 +0000

    simplify day 2

---
+++ 2021/src/02.ts
@@ -0,0 +1,57 @@
+import { getFileLines } from "./utils/input";
+
+const lines = getFileLines("02");
+
+const navigateSumbmarine = (useAim: boolean) => {
+  let height = 0;
+  let distance = 0;
+  let aim = 0;
+
+  lines.forEach((line) => {
+    if (line === "") {
+      return;
+    }
+    const [direction, magnitude] = line.split(" ");
+    const magNum = parseInt(magnitude);
+
+    if (useAim) {
+      switch (direction) {
+        case "forward":
+          distance += magNum;
+          height += aim! * magNum;
+          break;
+        case "up":
+          aim! -= magNum;
+          break;
+        case "down":
+          aim! += magNum;
+          break;
+        default:
+          throw new Error(
+            `Cannot process unrecognised direction '${direction}'`
+          );
+      }
+    } else {
+      switch (direction) {
+        case "forward":
+          distance += magNum;
+          break;
+        case "up":
+          height -= magNum;
+          break;
+        case "down":
+          height += magNum;
+          break;
+        default:
+          throw new Error(
+            `Cannot process unrecognised direction '${direction}'`
+          );
+      }
+    }
+  });
+
+  return height * distance;
+};
+
+console.log(`2.1: ${navigateSumbmarine(false)}`);
+console.log(`2.2: ${navigateSumbmarine(true)}`);
