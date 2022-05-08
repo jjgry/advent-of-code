06402ddbb09634300f49e2a16d948ebf34e6cebe
JJ Gray <git@jjgry.com>
2022-05-08 01:00:28 +0100

    day 1 part 2

--- 2015/src/01.ts
+++ 2015/src/01.ts
@@ -1,18 +1,22 @@
 import { getFileLines } from "./utils/input";
 
-const lines = getFileLines("01");
+const input = getFileLines("01")[0];
 
 export const day01part1 = () => {
-  const input = lines[0];
-
   let floor = 0;
   for (const char of input) {
-    if (char === "(") {
-      floor++;
-    }
-    if (char === ")") {
-      floor--;
-    }
+    if (char === "(") floor++;
+    if (char === ")") floor--;
   }
   return floor;
+};
+
+export const day01part2 = () => {
+  let floor = 0;
+  for (let i = 0; i < input.length; i++) {
+    if (input[i] === "(") floor++;
+    if (input[i] === ")") floor--;
+    if (floor < 0) return i + 1;
+  }
+  return -1;
 };
--- 2015/src/index.ts
+++ 2015/src/index.ts
@@ -1,7 +1,8 @@
-import { day01part1 } from "./01";
+import { day01part1, day01part2 } from "./01";
 
 const main = () => {
   console.log(`1.1: ${day01part1()}`);
+  console.log(`1.2: ${day01part2()}`);
 };
 
 main();
