786481438d634d2cf0e1720617f41bc69a58b7a3
JJ Gray <jjgray@scottlogic.com>
2025-12-04 16:54:03 +0000

    experiment with named returns

--- 2025/day04/day04.go
+++ 2025/day04/day04.go
@@ -15,13 +15,12 @@ func Run(input []string) {
 	fmt.Println("Part 2:", part2(grid))
 }
 
-func part1(grid [][]rune) int {
-	_, count := newGridAndCount(grid)
-	return count
+func part1(grid [][]rune) (count int) {
+	_, count = newGridAndCount(grid)
+	return
 }
 
-func part2(grid [][]rune) int {
-	count := 0
+func part2(grid [][]rune) (count int) {
 	for {
 		newGrid, newCount := newGridAndCount(grid)
 		if newCount == 0 {
@@ -30,14 +29,13 @@ func part2(grid [][]rune) int {
 		grid = newGrid
 		count += newCount
 	}
-	return count
+	return
 }
 
-func newGridAndCount(grid [][]rune) ([][]rune, int) {
+func newGridAndCount(grid [][]rune) (newGrid [][]rune, count int) {
 	height := len(grid)
 	width := len(grid[0])
-	newGrid := copy2dSlice(grid)
-	count := 0
+	newGrid = copy2dSlice(grid)
 	for h := range height {
 		for w := range width {
 			if grid[h][w] != '@' {
@@ -63,15 +61,14 @@ func newGridAndCount(grid [][]rune) ([][]rune, int) {
 			}
 		}
 	}
-	return newGrid, count
+	return
 }
 
-func copy2dSlice(grid [][]rune) [][]rune {
-	var gridCopy [][]rune
+func copy2dSlice(grid [][]rune) (gridCopy [][]rune) {
 	for _, row := range grid {
 		rowCopy := make([]rune, len(row))
 		copy(rowCopy, row)
 		gridCopy = append(gridCopy, rowCopy)
 	}
-	return gridCopy
+	return
 }
